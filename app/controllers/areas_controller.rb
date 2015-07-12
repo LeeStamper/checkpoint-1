87e60fa811d2ce8e1085b0009ad4e18452b3063c
Lee Stamper <lee@overshopper.com>
2015-07-11 23:27:07 -0500

    solution

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -6,19 +6,28 @@ class AreasController < ApplicationController
 
   def circle
     pi = 3.1416 # Close enough.
-    @r = params[:radius]
+    @r = params[:radius].to_f
+    @area = pi*@r**2
   end
 
   def triangle
     @b = params[:base].to_f
     @h = params[:vertical_height].to_f
-    area = 0.5 * b * h
+    @area = 0.5 * @b * @h
   end
 
   def rectangle
     @w = params[:width].to_f
     @h = params[:height].to_f
     @area = @w * @h
+  end
+
+  def trapezoid
+    @a = params[:a].to_f
+    @b = params[:b].to_f
+    @vertical_height = params[:vertical_height].to_f
+    @area = (@a + @b)*@vertical_height/2
+
   end
 
   def ellipse
@@ -30,5 +39,6 @@ class AreasController < ApplicationController
 
   def random
     @radii = [rand(1..20), rand(1..20), rand(1..20), rand(1..20), rand(1..20)]
+
   end
 end
